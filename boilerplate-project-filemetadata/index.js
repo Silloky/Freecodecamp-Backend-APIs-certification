58251a7f970c0fd9c149cac2010f3442630eeb79
Silloky (Elias K.) <ek.mindstorms@outlook.com>
2025-02-19 16:30:16 +0100

    fully working filemetadata project

--- boilerplate-project-filemetadata/index.js
+++ boilerplate-project-filemetadata/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const multer = require('multer');
 var cors = require('cors');
 require('dotenv').config()
 
@@ -11,8 +12,10 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  res.json({ name: originalname, type: mimetype, size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
